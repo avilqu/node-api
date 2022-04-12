5ea3060adcbc9515898d02e884e7991104a50c83
Adri Vilba <avilqu@gmail.com>
2022-04-12 21:16:11 +1200

    removed oauth special case from error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-    // Redirect to front-end if OAuth flow error
-    if (err.message === 'OAUTH_ERR_EXISTING_USER') {
-        res.redirect(302, '/auth');
-    }
-
     next(
         res.json({
             status: 'error',
